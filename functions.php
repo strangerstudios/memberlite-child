bcb7d35c83e3b196aa2db7847357a80cef5c97c7
Kim Coleman <kim@strangerstudios.com>
2025-04-16 12:04:18 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,20 +13,3 @@ function memberlite_child_enqueue_styles() {
     wp_enqueue_style( 'memberlite', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'memberlite_child_enqueue_styles' );
-
-/**
- * Allow child theme to inherit parent theme settings - based on code by @greenshady from https://core.trac.wordpress.org/ticket/27177#comment:14
- *
- */
-function memberlite_child_switch_theme_update_mods() {
-	if ( is_child_theme() && false === get_theme_mods() ) {
-		$mods = get_option( 'theme_mods_' . get_option( 'template' ) );
-		if ( false !== $mods ) {
-			foreach ( (array) $mods as $mod => $value ) {
-				if ( 'sidebars_widgets' !== $mod )
-					set_theme_mod( $mod, $value );
-			}
-		}
-	}
-}
-add_action( 'switch_theme', 'memberlite_child_switch_theme_update_mods' );
