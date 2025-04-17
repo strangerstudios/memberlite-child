588d7f8735d8bb41663b91fe5c0bcd22f1911c8e
Kim Coleman <kim@strangerstudios.com>
2025-04-17 07:14:32 -0400

    Updating the screenshot; adding version number to stylesheet enqueue

--- functions.php
+++ functions.php
@@ -10,6 +10,6 @@
  *
  */
 function memberlite_child_enqueue_styles() {
-    wp_enqueue_style( 'memberlite', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'memberlite', get_template_directory_uri() . '/style.css', array(), MEMBERLITE_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'memberlite_child_enqueue_styles' );
