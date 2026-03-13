9fb7a760e4b637f7e16cc845856c1ae501e3d616
Kim Coleman <kim@strangerstudios.com>
2026-03-13 15:40:50 -0400

    Fixing pmpro reference

--- functions.php
+++ functions.php
@@ -12,6 +12,6 @@ define( 'MEMBERLITE_CHILD_VERSION', '1.0' );
  */
 function memberlite_child_enqueue_styles() {
 	wp_enqueue_style( 'memberlite_style', get_template_directory_uri() . '/style.css', array( 'memberlite_main_style' ), MEMBERLITE_VERSION );
-	wp_enqueue_style( 'memberlite_pmpro_style', get_stylesheet_directory_uri() . '/style.css', array( 'memberlite_style' ), MEMBERLITE_CHILD_VERSION );
+	wp_enqueue_style( 'memberlite_child_style', get_stylesheet_directory_uri() . '/style.css', array( 'memberlite_style' ), MEMBERLITE_CHILD_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'memberlite_child_enqueue_styles' );
