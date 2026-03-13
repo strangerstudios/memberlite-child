098f0f8a3f52125dbb08dd9a67de4002a1f64ef2
Kim Coleman <kim@strangerstudios.com>
2026-03-13 15:19:04 -0400

    Updating child theme template strucutre for Memberlite 7.0

--- functions.php
+++ functions.php
@@ -4,12 +4,14 @@
  *
  * @package Memberlite Child
  */
- 
+
+define('MEMBERLITE_CHILD_VERSION', '1.0');
+
 /**
  * Enqueue scripts and styles.
- *
  */
 function memberlite_child_enqueue_styles() {
-    wp_enqueue_style( 'memberlite', get_template_directory_uri() . '/style.css', array(), MEMBERLITE_VERSION );
+	wp_enqueue_style('memberlite_style', get_template_directory_uri() .'/style.css', array('memberlite_main_style'), MEMBERLITE_VERSION);
+	wp_enqueue_style('memberlite_pmpro_style', get_stylesheet_directory_uri() .'/style.css', array('memberlite_style'), MEMBERLITE_CHILD_VERSION);
 }
 add_action( 'wp_enqueue_scripts', 'memberlite_child_enqueue_styles' );
